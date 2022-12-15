6258ee6ce9b376b91a0f4270d28898dbee630074
Nikita Pashin <luletzkiy@gmail.com>
2022-12-15 16:24:20 +0300

    Site Template

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/contacts/')
+def contacts():
+    developer_name = "Nick"
+    context = {'name': developer_name}
+    return render_template('contacts.html', props=context)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
